4d3fec0925db886311acb77f8b852bdafda96340
imdemogithub <imdemoemail@gmail.com>
2021-07-14 14:51:37 +0530

    Django ToDo

--- ToDo/settings.py
+++ ToDo/settings.py
@@ -119,8 +119,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+import os
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'chhatani.jaykishan@gmail.com'
+EMAIL_HOST_PASSWORD = 'cevrylfjzgmijvij'
